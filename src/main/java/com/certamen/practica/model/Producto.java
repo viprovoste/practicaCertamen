9b48f9e06794006fd8f3fdaa2ba39b012a73219f
vicente <vi.provoste@duocuc.cl>
2026-04-09 11:44:41 -0400

    modelo

---
+++ src/main/java/com/certamen/practica/model/Producto.java
@@ -0,0 +1,15 @@
+package com.certamen.practica.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Producto {
+    private String nombre;
+    private int precio;
+    private String categoria;
+    
+}
